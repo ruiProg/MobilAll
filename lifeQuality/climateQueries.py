fc101b7ddffd6d8b5e6b5a34106fc805bdebf656
ruiProg <ruifilipeosorio17@hotmail.com>
2017-11-28 16:22:40 +0000

    city Climate

--- lifeQuality/climateQueries.py
+++ lifeQuality/climateQueries.py
@@ -3,25 +3,32 @@ import util, mobilAll
 
 climateQueries_api = Blueprint('climateQueries_api', __name__)
 
-Climate queries
+#Climate queries
 
-#
+#find climate in a city
 @climateQueries_api.route('/api/cityClimate')
-def countriesList():
-	maxCountries = 250
-	nameSort = int(request.args.get('sort', '0'))
+def placeClimate():
+	place = request.args.get('place', '')
+	offset = int(request.args.get('from', '0'))
+	size = int(request.args.get('size', util.defaultSize))
 	query = {
-		"size": 0, #don't retrieve hits
-		"aggs" : {
-			"countries" : {
-				"terms" : {
-					"field" : "country.keyword",
-					"size": maxCountries
-				}
+		"from" : offset, 
+		"size" : size,
+		"query" : {
+			"has_parent" : {
+				"parent_type": "city", 
+				"query" :{
+					"multi_match" : {
+						"query": place,
+						"fields": ["univRegion", "regionName"]
+					}
+				},
+				"inner_hits": { "_source" : ["univRegion", "regionName"]} 
 			}
 		}
 	}
-	if nameSort > 0:
-		query['aggs']['countries']['terms']['order'] = { "_key" : "asc" }
-	res = util.es.search(index=util.univsIndex, body=query)
-	return jsonify(res['aggregations']['countries']['buckets'])
+	res = util.es.search(index=util.climateIndex, body=query)
+	if res['hits']['hits']:
+		return jsonify({"nbItems" : res['hits']['total'], "items": [item if util.debug else item['_source'] for item in res['hits']['hits']]})
+	else:
+		return 'No item found'
