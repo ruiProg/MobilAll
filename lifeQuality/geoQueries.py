dcb0b7e5be7a293c7daf6da8b87d5667135b517c
ruiProg <ruifilipeosorio17@hotmail.com>
2017-11-28 00:02:36 +0000

    close cities

--- lifeQuality/geoQueries.py
+++ lifeQuality/geoQueries.py
@@ -54,7 +54,7 @@ def citiesList():
 	res = util.es.search(index=util.univsIndex, body=query)
 	return jsonify(res['aggregations']['cities']['buckets'])
 
-
+#list of states with universities
 @geoQueries_api.route('/api/states')
 def statesList():
 	maxStates = 60
@@ -75,6 +75,7 @@ def statesList():
 	res = util.es.search(index=util.univsIndex, body=query)
 	return jsonify(res['aggregations']['states']['buckets'])
 
+#list of cities in one country
 @geoQueries_api.route('/api/countryCities')
 def countryCities():
 	name = request.args.get('country', '')
@@ -93,4 +94,102 @@ def countryCities():
 	if res['hits']['hits']:
 		return jsonify({"nbItems" : res['hits']['total'], "items": [item if util.debug else item['_source'] for item in res['hits']['hits']]})
 	else:
-		return 'No cities found'
+		return 'No city found'
+
+#list of cities in one state
+@geoQueries_api.route('/api/stateCities')
+def stateCities():
+	name = request.args.get('state', '')
+	maxCities = 250
+	nameSort = int(request.args.get('sort', '0'))
+	query = {
+		"size": 0,
+		"query": {
+		  "match": {
+		    "state": name
+		  }
+		}, 
+		"aggs" : {
+			"cities" : {
+				"terms" : {
+					"field" : "city.keyword",
+					"size": maxCities
+				}
+			}
+		}
+	} 
+	if nameSort > 0:
+		query['aggs']['cities']['terms']['order'] = { "_key" : "asc" }
+	res = util.es.search(index=util.univsIndex, body=query)
+	if res['aggregations']['cities']['buckets']:
+		return jsonify(res['aggregations']['cities']['buckets'])
+	else:
+		return 'No city found'
+
+#find city
+@geoQueries_api.route('/api/city')
+def city():
+	name = request.args.get('name', '')
+	offset = int(request.args.get('from', '0'))
+	size = int(request.args.get('size', util.defaultSize))
+	query = {
+		"from": offset,
+		"size": size,
+		"query": {
+			"match": {
+				"city": name
+			}
+		}
+	} 
+	res = util.es.search(index=util.citiesIndex, body=query)
+	if res['hits']['hits']:
+		return jsonify({"nbItems" : res['hits']['total'], "items": [item if util.debug else item['_source'] for item in res['hits']['hits']]})
+	else:
+		return 'No city found'
+
+#find close cities to a certain point
+@geoQueries_api.route('/api/closeCities')
+def closeCities():
+	latVar = float(request.args.get('lat', '0.0'))
+	lonVar = float(request.args.get('lon', '0.0'))
+	print(lonVar)
+	distance = float(request.args.get('distance', '10.0'))
+	offset = int(request.args.get('from', '0'))
+	size = int(request.args.get('size', util.defaultSize))
+	query = {
+		"from": offset,
+		"size": size,
+	    "query": {
+	        "bool" : {
+	            "must" : {
+	                "match_all" : {}
+	            },
+	            "filter" : {
+	                "geo_distance" : {
+	                    "distance" : str(distance)+"km",
+	                    "location" : {
+	                        "lat" : latVar,
+	                        "lon" : lonVar
+	                    }
+	                }
+	            }
+	        }
+	    },
+	    "sort" : [
+	    {
+	    	"_geo_distance" : {
+	    		"location" : {
+	    			"lat" : latVar,
+	    			"lon" : lonVar
+	    		},
+	    		"order" : "asc",
+	    		"unit" : "km",
+	    		"mode" : "min"
+	    	}
+	    }]
+	}
+	res = util.es.search(index=util.citiesIndex, body=query)
+	if res['hits']['hits']:
+		return jsonify({"nbItems" : res['hits']['total'], "items": [item if util.debug else item['_source'] for item in res['hits']['hits']]})
+	else:
+		return 'No city found'
--- lifeQuality/mapping.py
+++ lifeQuality/mapping.py
@@ -1,2 +1,18 @@
+import util
+
+def createCityMapping():
+	doc = {
+    	"mappings": {
+        	"doc": {
+            	"properties": {
+                	"location": {
+                    	"type": "geo_point"
+                	}
+            	}
+        	}
+     	}
+	}
+	util.es.indices.create(index=util.citiesIndex, body=doc)
+
 def createMappings():
-	pass
+	createCityMapping()
