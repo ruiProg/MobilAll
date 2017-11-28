0c1c49cbea2f021bb1be187bb27de052616525c6
DianaP5 <digpinto@gmail.com>
2017-11-28 14:29:42 +0000

    add Query

--- lifeQuality/priceQueries.py
+++ lifeQuality/priceQueries.py
@@ -56,3 +56,58 @@ def itemPricebyPlace():
 	else:
 		return 'No item found'
 
+@priceQueries_api.route('/api/placePrices')
+def placePrices():
+	place = request.args.get('place', '')
+	offset = int(request.args.get('from', '0'))
+	size = int(request.args.get('size', util.defaultSize))
+	query = {
+		"from" : offset, 
+		"size" : size,
+		"query" : {
+			"has_parent" : {
+				"parent_type": "region", 
+				"query": {
+					"match": { "univRegion": place}
+				},
+				"inner_hits": { "_source" : ["univRegion"]} 
+			}
+		}
+	}
+	res = util.es.search(index=util.pricesIndex, body=query)
+	if res['hits']['hits']:
+		return jsonify({"nbItems" : res['hits']['total'], "items": [item if util.debug else item['_source'] for item in res['hits']['hits']]})
+	else:
+		return 'No item found'
+
+@priceQueries_api.route('/api/categorybyPlace')
+def categorybyPlace():
+	category = request.args.get('category', '')
+	place = request.args.get('place', '')
+	offset = int(request.args.get('from', '0'))
+	size = int(request.args.get('size', util.defaultSize))
+	query = {
+		"from" : offset, 
+		"size" : size,
+		"query" : { 
+			"bool" : {
+				"must" : [
+					{"match": {"category": category}},
+					{
+						"has_parent" : {
+							"parent_type": "region",
+							"query": {
+								"match": { "univRegion": place}
+							}, 
+							"inner_hits": { "_source" : ["univRegion"]} 
+						}
+					}
+				]
+			}
+		}
+	}
+	res = util.es.search(index=util.pricesIndex, body=query)
+	if res['hits']['hits']:
+		return jsonify({"nbItems" : res['hits']['total'], "items": [item if util.debug else item['_source'] for item in res['hits']['hits']]})
+	else:
+		return 'No item found'
