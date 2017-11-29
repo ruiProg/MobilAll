b2eb58741fd0016b04deb7ef3aaf361c357191e8
ruiProg <ruifilipeosorio17@hotmail.com>
2017-11-29 11:42:06 +0000

    sort indice factors

--- lifeQuality/indicesQueries.py
+++ lifeQuality/indicesQueries.py
@@ -2,6 +2,22 @@ from flask import Blueprint, jsonify, request
 import util, mobilAll, json
 
 indicesQueries_api = Blueprint('indicesQueries_api', __name__)
+indexFactors = {
+	"crime": "crime_index", 
+	"traffictime" : "traffic_time_index",
+	"cpirent": "cpi_and_rent_index",
+	"purchasingpower" : "purchasing_power_incl_rent_index",
+	"restaurant" :  "restaurant_price_index",
+	"safety" : "safety_index",
+	"co2" : "traffic_co2_index",
+	"cpi" : "cpi_index",
+	"quality" : "quality_of_life_index",
+	"rent" : "rent_index",
+	"healthcare" : "health_care_index",
+	"traffic" : "traffic_index",
+	"groceries" : "groceries_index",
+	"pollution" : "pollution_index"
+}
 
 #Indices queries
 
@@ -29,3 +45,25 @@ def placeIndices():
 		return jsonify({"nbItems" : res['hits']['total'], "items": [item if util.debug else item['_source'] for item in res['hits']['hits']]})
 	else:
 		return "Place not found"
+
+#Sort by index
+@indicesQueries_api.route('/api/sortIndices')
+def sortIndices():
+	sortVal = request.args.get('sortVal', '0')
+	offset = int(request.args.get('from', '0'))
+	size = int(request.args.get('size', util.defaultSize))
+	query = {
+		"from" : offset, 
+		"size" : size,
+		"query" : {
+			"match_all" : {}
+		}
+	}
+	if sortVal in indexFactors:
+		query['sort'] = {indexFactors[sortVal] : {"order" : "desc"}}
+	print(query)
+	res = util.es.search(index=util.indicesIndex, body=query)
+	if res['hits']['hits']:
+		return jsonify({"nbItems" : res['hits']['total'], "items": [item if util.debug else item['_source'] for item in res['hits']['hits']]})
+	else:
+		return 'No item found'
